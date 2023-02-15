94a8cef713110f95da1685e1ca56b79335de9f10
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-15 19:36:52 +0100

    fix remove placeholder for login

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
 import { PlaceholderDirective } from './placeholder/placeholder.directive';
 import { DropdownDirective } from './dropdown.directive';
 import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
-  declarations: [AlertComponent, DropdownDirective],
+  declarations: [AlertComponent, PlaceholderDirective, DropdownDirective],
   imports: [CommonModule],
-  exports: [AlertComponent, DropdownDirective, CommonModule],
+  exports: [
+    AlertComponent,
+    PlaceholderDirective,
+    DropdownDirective,
+    CommonModule
+  ],
   entryComponents: [AlertComponent]
 })
 export class SharedModule {}
