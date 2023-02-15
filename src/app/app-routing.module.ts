564f64b1dd2a415fe0a6a0e65a204a38c1ebd1dd
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-15 19:25:34 +0100

    update root routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   {
     path: 'login',
     loadChildren: () =>
       import('./pages/login/login.module').then((m) => m.LoginModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () =>
+      import('./pages/register/register.module').then((m) => m.RegisterModule)
   }
-  // {
-  //   path: 'register',
-  //   loadChildren: () =>
-  //     import('./pages/register/register.module').then((m) => m.RegisterModule)
-  // }
 ];
 
 @NgModule({
