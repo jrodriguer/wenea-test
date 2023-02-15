1dfd7c9751f0e4d9a41f6f2ecc8a7389210de270
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-15 17:29:38 +0100

    wip main routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,12 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () =>
       import('./pages/login/login.module').then((m) => m.LoginModule)
-  },
-  {
-    path: 'register',
-    loadChildren: () =>
-      import('./pages/register/register.module').then((m) => m.RegisterModule)
   }
+  // {
+  //   path: 'register',
+  //   loadChildren: () =>
+  //     import('./pages/register/register.module').then((m) => m.RegisterModule)
+  // }
 ];
 
 @NgModule({
