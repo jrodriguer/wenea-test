1d15955c47c1639d942534df31e1c79e1c5676eb
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-15 17:37:03 +0100

    refactor user model for get private property

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,12 +4,16 @@ export class User {
   constructor(
     public address: Address,
     public email: string,
-    public id: string,
+    public uid: string,
     public name: string,
-    private _token: string,
-    private _tokenExpirationDate: Date,
+    private _token?: string,
+    private _tokenExpirationDate?: Date,
     public password?: string
   ) {}
+
+  get tokenExpirationDate() {
+    return this._tokenExpirationDate;
+  }
 
   get token() {
     if (!this._tokenExpirationDate || new Date() > this._tokenExpirationDate) {
