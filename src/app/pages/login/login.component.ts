2193f5fddf63ecfd36e152f63fd1499740a61f3f
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-15 03:09:25 +0100

    WIP sign in logic - maked display and styles

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ComponentFactoryResolver,
+  ViewChild,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Observable, Subscription, ReplaySubject, takeUntil } from 'rxjs';
+
+import { AlertComponent } from '../../shared/alert/alert.component';
+import { AuthResponseData } from '../../../models/auth-model.temp';
+import { AuthService } from '../../auth/auth.service';
+import { PlaceholderDirective } from '../../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit, OnDestroy {
+  public isLoading = false;
+  private destroyed$: ReplaySubject<boolean> = new ReplaySubject(1);
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective =
+    {} as PlaceholderDirective;
 
+  constructor(
+    private authSrv: AuthService,
+    private router: Router,
+    private componentFactoryResolver: ComponentFactoryResolver
+  ) {}
+
+  ngOnInit() {}
+
+  ngOnDestroy() {
+    this.destroyed$.next(true);
+    this.destroyed$.complete();
+  }
+
+  public onSubmit(form: NgForm) {
+    const email = form.value.email;
+    const pw = form.value.password;
+
+    // let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+
+    // authObs = this.authSrv.signIn(email, pw);
+    // authObs.pipe(takeUntil(this.destroyed$)).subscribe(
+    //   (res) => {
+    //     this.isLoading = false;
+    //     // this.router.navigate(['/recipes']);
+    //   },
+    //   (err) => {
+    //     this.isLoading = false;
+    //     this.showErrorAlert(err);
+    //   }
+    // );
+    form.reset();
+  }
+
+  private showErrorAlert(message: string) {
+    const alertFactory =
+      this.componentFactoryResolver.resolveComponentFactory(AlertComponent);
+
+    const hostViewContainerRef = this.alertHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(alertFactory);
+
+    componentRef.instance.msg = message;
+    componentRef.instance.close
+      .pipe(takeUntil(this.destroyed$))
+      .subscribe(() => {
+        hostViewContainerRef.clear();
+      });
+  }
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { LoginComponent } from './login.component';
-
-
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
+  declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    RouterModule.forChild([{ path: '', component: LoginComponent }])
   ]
 })
-export class LoginModule { }
+export class LoginModule {}
