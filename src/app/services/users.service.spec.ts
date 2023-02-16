b39ebd7b5c47dc01f26ba8348aa71b2b2d629932
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-16 04:09:22 +0100

    unit testing getUsers() method of users.servicefor

--- src/app/services/users.service.spec.ts
+++ src/app/services/users.service.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 
 import { UsersService } from './users.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UsersService]
+    });
+
     service = TestBed.inject(UsersService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getUsers()', () => {
+    it('should return an Observable of users', () => {
+      const mockUsers = [
+        { id: 1, name: 'User 1' },
+        { id: 2, name: 'User 2' }
+      ];
+
+      service.getUsers().subscribe((users) => {
+        expect(users.length).toBe(2);
+        expect(users).toEqual(mockUsers);
+      });
+
+      const req = httpMock.expectOne(`${service.baseUrl}/users`);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockUsers);
+    });
   });
 });
