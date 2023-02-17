1c7f8af0e375f2cf5d7a82d6544aaa50b0df15b0
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-02-17 21:00:01 +0100

    call sign out into dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../../auth/auth.service';
 import { UserService } from '../../services/user.service';
 
 @Component({
@@ -8,9 +9,16 @@ import { UserService } from '../../services/user.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.userService.getUsers().subscribe((user) => console.log(user));
+  }
+
+  onSignOut() {
+    this.authService.logout();
   }
 }
