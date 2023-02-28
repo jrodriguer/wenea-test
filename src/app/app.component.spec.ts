04ea595122ae0c01e075445c9c439ce15c5e6908
juliorr <juliorodriguezramirez@gmail.com>
2023-03-01 00:36:25 +0100

    fix imports models for testing root app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import 'firebase/firestore';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
+import { AuthService } from './auth/auth.service';
+import { environment } from '../environments/environment';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        HttpClientTestingModule,
+      ],
+      providers: [AuthService],
       declarations: [AppComponent]
     }).compileComponents();
   });
