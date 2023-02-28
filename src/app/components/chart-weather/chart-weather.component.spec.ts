2c307ba3cd776fc172882bb8e1bac523eecaa8d9
juliorr <juliorodriguezramirez@gmail.com>
2023-02-28 23:25:13 +0100

    fix imports moduels for testing chart-weather component

--- src/app/components/chart-weather/chart-weather.component.spec.ts
+++ src/app/components/chart-weather/chart-weather.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NgChartsModule } from 'ng2-charts';
 
 import { ChartWeatherComponent } from './chart-weather.component';
 
@@ -8,6 +10,7 @@ describe('ChartWeatherComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [NgChartsModule, HttpClientTestingModule],
       declarations: [ChartWeatherComponent]
     }).compileComponents();
 
