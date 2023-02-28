b1896095f279f3cba9b732bbc3efbabe04bae233
juliorr <juliorodriguezramirez@gmail.com>
2023-02-28 23:48:42 +0100

    function to isolated route guard test utility and remove configureTestingModule

--- src/app/auth/auth.guard.spec.ts
+++ src/app/auth/auth.guard.spec.ts
@@ -1,13 +1,25 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
+import { RouterStateSnapshot } from '@angular/router';
 
 import { AuthGuard } from './auth.guard';
+import { AuthService } from './auth.service';
 
+function fakeRouterState(url: string): RouterStateSnapshot {
+  return {
+    url,
+  } as RouterStateSnapshot;
+}
+
+// When AuthGuard guards a CanActivate hook, access to the route is rejected
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let serviceStub: Partial<AuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    guard = TestBed.inject(AuthGuard);
+    serviceStub = {};
+    guard = new AuthGuard(serviceStub as AuthService, routerSpy);
   });
 
   it('should be created', () => {
