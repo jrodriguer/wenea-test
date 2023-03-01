1eaa208bd6743206c9f6ef597687d22074ac500f
juliorr <juliorodriguezramirez@gmail.com>
2023-03-01 13:52:19 +0100

    fix configure testing module for login page

--- src/app/pages/login/login.component.spec.ts
+++ src/app/pages/login/login.component.spec.ts
@@ -1,5 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import 'firebase/firestore';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { FormsModule } from '@angular/forms';
 
+import { environment } from '../../../environments/environment';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
@@ -8,6 +17,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        HttpClientTestingModule,
+        HttpClientTestingModule,
+        FormsModule
+      ],
       declarations: [LoginComponent]
     }).compileComponents();
 
