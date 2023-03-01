a26e12462de94504b52c97ded025c15b777dd188
juliorr <juliorodriguezramirez@gmail.com>
2023-03-01 13:43:29 +0100

    fix configure testing module for auth service

--- src/app/auth/auth.service.spec.ts
+++ src/app/auth/auth.service.spec.ts
@@ -1,12 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import 'firebase/firestore';
 
+import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        HttpClientTestingModule
+      ],
+      providers: [AuthService]
+    });
     service = TestBed.inject(AuthService);
   });
 
