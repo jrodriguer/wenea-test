880a4182be085fe0a988239bf862f2a308c7ac07
JulioRodriguezR <juliorodriguezramirez@gmail.com>
2023-03-03 18:29:59 +0100

    use behaviorSubject value for address

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,25 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Router } from '@angular/router';
+import { filter, Subject, takeUntil } from 'rxjs';
 
 import { AuthService } from '../../auth/auth.service';
-import { UserService } from '../../services/user.service';
 import { ModalDialogComponent } from '../../components/modal-dialog/modal-dialog.component';
+import { User } from 'src/models/user.model';
+import { Observable } from '@firebase/util';
+import { Address } from 'src/models/ddbb.model';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+  public userLogued$: any;
+  private destroyed$ = new Subject<void>();
+  public address: Address | undefined;
+
   constructor(
-    private userService: UserService,
     private authService: AuthService,
     private modalService: NgbModal,
     private router: Router
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userLogued$ = this.authService.user$.pipe(filter((acc) => !!acc));
+    this.userLogued$.pipe(takeUntil(this.destroyed$)).subscribe((v: User) => {
+      this.address = v.address;
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
 
   onSignOut() {
     this.authService.logout();
