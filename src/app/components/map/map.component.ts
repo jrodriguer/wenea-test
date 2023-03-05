4eae3a0019b605dfcaa900ad6c09bf288bda81a8
Julio <46267103+JulioRodriguezR@users.noreply.github.com>
2023-03-05 05:48:50 +0100

    spain geographical coordinates

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -15,11 +15,11 @@ export class MapComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    this.map = L.map('map').setView([51.505, -0.09], 13);
+    this.map = L.map('map').setView([40.45, -3.8], 5);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
-      maxZoom: 18,
+      maxZoom: 19,
       tileSize: 512,
       zoomOffset: -1
     }).addTo(this.map);
